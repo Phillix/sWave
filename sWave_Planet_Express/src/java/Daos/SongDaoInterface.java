4b3a88e4322b250fa4fca0efdff38641ee7be8dd
AustinFoley96 <austinfoley96@gmail.com>
2016-02-18 12:55:29 +0000

    Created SongDaoInterface

---
+++ sWave_Planet_Express/src/java/Daos/SongDaoInterface.java
@@ -0,0 +1,12 @@
+package Daos;
+
+import Dtos.Song;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Austin
+ */
+public interface SongDaoInterface {
+    public ArrayList<Song> getAllSongs();
+}

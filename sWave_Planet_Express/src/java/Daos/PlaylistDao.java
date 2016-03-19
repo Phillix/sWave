8245b5c24f1fa210bff0035034a477d15e8fa751
Phillix <Phillix@Phillix-PC>
2016-03-19 12:16:50 +0000

    Added playlist dao --no tests

---
+++ sWave_Planet_Express/src/java/Daos/PlaylistDao.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Daos;
+
+/**
+ *
+ * @author Phillix
+ */
+public class PlaylistDao extends Dao {
+    
+    private final boolean DEBUG = sWave.Server.DEBUGGING;
+
+    private final String TABLE_NAME  = "PLAYLISTS";
+    private final String ID          = "PLAYLISTID";
+    private final String USERID      = "USERID";
+    private final String TITLE       = "TITLE";
+    
+    
+}

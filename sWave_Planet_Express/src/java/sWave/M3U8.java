5af60fe76f4d48a8a77a8137702f7486c5174f88
Brian Millar <amb3r@protonmail.ch>
2016-03-22 14:02:28 +0000

    Finished Basic M3U8 Support

--- sWave_Planet_Express/src/java/sWave/M3U8.java
+++ sWave_Planet_Express/src/java/sWave/M3U8.java
@@ -8,11 +8,18 @@ import java.util.ArrayList;
  * @author Brian Millar
  */
 public class M3U8 {
-    private String playlistFile;
+    private String fileContents;
     private ArrayList<Song> playlist;
     
     public M3U8(ArrayList<Song> playlist) {
-        playlistFile = "#EXTM3U\n";
+        fileContents = "#EXTM3U\n";
         this.playlist = playlist;
+        playlist.stream().forEach((Song s) -> {
+            fileContents += "#EXTINF:" +
+                            s.getDuration() + ","   +
+                            s.getArtist()   + " - " +
+                            s.getTitle()    + '\n'  +
+                            s.getFilename() + '\n';
+        });
     }
 }

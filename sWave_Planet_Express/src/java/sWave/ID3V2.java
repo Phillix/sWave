3478644a8598f3ecfddcbdb7c22bf21c4f0b5394
Brian Millar <amb3r@protonmail.ch>
2016-03-29 16:15:06 +0100

    More ID3V2 Work

--- sWave_Planet_Express/src/java/sWave/ID3V2.java
+++ sWave_Planet_Express/src/java/sWave/ID3V2.java
@@ -1,7 +1,6 @@
 package sWave;
 
 import Dtos.Song;
-import java.util.Arrays;
 
 /**
  *
@@ -9,21 +8,16 @@ import java.util.Arrays;
  */
 public class ID3V2 {
 
-    /**
-     * 
-     * @param song
-     * @return a song object with metadata set
-     */
     public static void extractTags(Song song) {
         /*
-            This method extracts ID3 metadata from the MP3 data in a Song object 
-            and returns a Song object with the same MP3 data but with the other 
-            fields filled with the information found in the ID3 metadata.
+            This method extracts ID3V2.3 metadata from the MP3 data in a Song 
+            object passed in and fills its fields with the information found in 
+            the ID3 metadata.
         
             I built this ID3 extractor from the documentaion of the ID3 spec 
-            found at id3.org. It is a basic implementation of ID3V2.3 that only 
-            extracts the information we need for our project, irrelevant data is 
-            simply ignored.
+            found at id3.org. It is a stripped down, basic implementation of 
+            ID3V2.3 that only extracts the information we need for our project, 
+            irrelevant data is simply ignored.
         */
         
         /*
@@ -56,44 +50,89 @@ public class ID3V2 {
                 Most of this is useless anyway, I couldn't find a single MP3 
                 that actually had any of these flags set...
 
-                Anyway the next 4 bytes describe the tag size:
-                We don't need this information yet so lets skip it.
+                Anyway the next 4 bytes describe the tag size,
+                the size is the total size of the ID3 tag excluding the header 
+                so it should be the total - 10 bytes. The max is 256MB, however 
+                we do not accept files greater than 16MB.
             */
             
-            //Lets do a quick and dirty search for tags we want
-            //Replace buffer.length later with the actual tag length
-            
-            //Temp hacking
-            
-            for (int i = 11; i < buffer.length - 4; i += 4) {
-                String frame = "" + (char)buffer[i] + (char)buffer[i + 1] + (char)buffer[i + 2] + (char)buffer[i + 3];
-                if (frame.equals("TIT2"))
-                    System.out.println("Title Found");
-                else if (frame.equals("TPE1"))
-                    System.out.println("Artist Found");
-                else if (frame.equals("TCON"))
-                    System.out.println("Genre Found");
-                else if (frame.equals("TLEN"))
-                    System.out.println("Duration Found");
-                else if (frame.equals("TYER"))
-                    System.out.println("Year Found");
-                else if (frame.equals("APIC"))
-                    System.out.println("Artwork Found");
-            }
-
-            //==================================================================
+            int totalSize  = 0;
+            int tagSize[][] = {dec2bin(buffer[6]), dec2bin(buffer[7]), dec2bin(buffer[8]), dec2bin(buffer[9])};
             
             /*
-                If the extended Header is not used we can start processing ID3V2 
-                frames from here. The frames we really want for sWave are:
-                    TIT2 - Title
-                    TPE1 - Artist
-                    TCON - First byte should give us the genre
-                    TLEN - Duration in millisecs
-                    
-                    TYER - Year
-                    WCOP/TCOP - Copyright Info
+                The size is represented across 4 bytes where the first bit of 
+                each is ignored, so we need to build the representation as an 
+                integer ourselves, it will be between 0 and 256MB as 7 bits is 
+                used across 4 bytes:
+
+                7*4 = 28 and 2^28 = 268435456b so 262144kb or 256mb.
+            
+                So we can represent 0 - 268435455b
             */
+
+            //Calculate the total tag size
+            for (int j = 0; j < 4; j++)
+                for (int i = 1; i <= 7; i++)
+                    if (tagSize[j][i] == 1)
+                        totalSize += Math.pow(2, (28 - (j * 7) - i));
+            
+            System.out.println("A " + (totalSize / 1024) + "kb ID3 Tag was found, attempting to extract data...");
+            
+        //==END=OF=HEADER=======================================================
+        
+        //==EXTENDED=HEADER=====================================================
+        
+            //Check that there is an extended header (there usually isn't)
+            if (extHeader == 1) {
+                
+            }
+        
+        //==END=OF=EXTENDED=HEADER==============================================
+        
+        //==DECLARATION=OF=DATA=WE=WANT=========================================
+        /*
+            String tags[][] = new String[7][2];
+            tags[0][0] = "TIT2"; //Title
+            tags[0][1] = "TPE1"; //Artist
+            tags[0][2] = ""; //Album
+            tags[0][3] = "TCON"; //First Byte should be Genre
+            tags[0][4] = "TLEN"; //Duration
+            tags[0][5] = "TYER"; //Year
+            tags[0][6] = "APIC"; //Artwork
+        */
+        
+        //==END=OF=DECLARATIONS=================================================
+        
+        //==ID3=FRAMES==========================================================
+        
+        //==END=OF=ID3=FRAMES===================================================
+        
+        //==SET=THE=DATA========================================================
+            
+            //Set the data we found
+            /*
+            if (tags[1][0] != null)
+                song.setTitle(tags[1][0]);
+            
+            if (tags[1][1] != null)
+                song.setArtist(tags[1][1]);
+
+            if (tags[1][2] != null)
+                song.setAlbum(tags[1][2]);
+            
+            if (tags[1][3] != null)
+                song.setGenre(tags[1][3]);
+            
+            if (tags[1][4] != null)
+                song.setDuration(tags[4]);
+            
+            if (tags[1][5] != null)
+                song.setYear(tags[1][5]);
+            
+            if (tags[1][6] != null)
+                song.setArtwork(tags[1][6]);
+            */
+        //==END=OF=DATA=SETTING=================================================
               
         }
     }

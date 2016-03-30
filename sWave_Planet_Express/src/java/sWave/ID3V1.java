270fcc73af7cdffadf3be10034ebafb16d2adb03
Brian Millar <amb3r@protonmail.ch>
2016-03-30 13:49:30 +0100

    Began ID3V1 Work

---
+++ sWave_Planet_Express/src/java/sWave/ID3V1.java
@@ -0,0 +1,20 @@
+package sWave;
+
+import Dtos.Song;
+
+/**
+ *
+ * @author Brian Millar
+ */
+public class ID3V1 {
+    /*
+        This will run first as a fallback if ID3V2 cannot find data, we will 
+        load data from here first if we can find it then any ID3V2 data found 
+        will overwrite what was found here.
+    */
+    
+    public static void extractTags(Song song) {
+
+    }
+    
+}

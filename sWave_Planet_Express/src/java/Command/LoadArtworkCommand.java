7fc62133bafbe77e5741d6d7a91e103965ff1ea5
Brian Millar <amb3r@protonmail.ch>
2016-04-05 01:45:31 +0100

    Important Artwork Loading Bug Fix

--- sWave_Planet_Express/src/java/Command/LoadArtworkCommand.java
+++ sWave_Planet_Express/src/java/Command/LoadArtworkCommand.java
@@ -18,19 +18,17 @@ public class LoadArtworkCommand implements Command {
 
     @Override
     public String executeCommand(HttpServletRequest request, HttpServletResponse response) {
-        System.out.println("???");
         try {
-            System.out.println("IM HERE");
             SongDao dao = new SongDao();
             Song s = dao.getSongById(Integer.parseInt(request.getParameter("songid")));
             OutputStream out = response.getOutputStream();
-            out.write(s.getArtwork());
-            System.out.println("FOUND: " + s.getArtwork().length);
-            out.flush();
+            if (s.getArtwork() != null) {
+                out.write(s.getArtwork());
+                out.flush();
+            }
             out.close();
             return null; //We don't want to redirect
         } catch (IOException ex) {
-            System.out.println("mmmm");
             if (DEBUG)
                 ex.printStackTrace();
         }

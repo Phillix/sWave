59e02bb0ddcff3524fda479a5c328d7175e9c70a
Phillix <D00159732@student.dkit.ie>
2016-04-20 15:14:52 +0100

    confirmFriendCommand

---
+++ sWave_Planet_Express/src/java/Command/ConfirmFriendCommand.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Command;
+
+import Daos.FriendDao;
+import Dtos.Friend;
+import Dtos.User;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author Phillix
+ */
+public class ConfirmFriendCommand implements Command {
+
+    @Override
+    public String executeCommand(HttpServletRequest request, HttpServletResponse response) {
+        FriendDao fd = new FriendDao();
+        HttpSession session = request.getSession();
+        User u = (User) session.getAttribute("user");
+        String friendId = request.getParameter("friendId");
+        
+        if(u != null && friendId != null && !friendId.isEmpty()) {
+            fd.confirmFriend(u.getUserId(), Integer.valueOf(friendId));
+            return "/account.jsp";
+        }
+        return "/error.jsp";
+    }
+    
+}
